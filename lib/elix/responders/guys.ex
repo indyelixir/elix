b10e628ca07fcc0406c2d0eb845f8b18d375fcdc
Steve Grossi <me@stevegrossi.com>
2016-10-09 12:53:16 -0400

    Patch Responders.Guys to avoid infinite loop
    Because it appears the `ignore_from_self?` setting isn’t working or was removed from Hedwig: https://github.com/hedwig-im/hedwig/search?utf8=%E2%9C%93&q=ignore_from_self%3F

--- lib/elix/responders/guys.ex
+++ lib/elix/responders/guys.ex
@@ -24,6 +24,18 @@ defmodule Elix.Responders.Guys do
   """
 
   hear ~r/\bguys\b/i, msg do
-    reply msg, "Terms like *guys* can feel exclusionary. Did you mean **" <> random(@forms_of_address) <> "**?"
+    unless from_self?(msg) do
+      response = "Terms like *guys* can feel exclusionary. Did you mean **" <> random(@forms_of_address) <> "**?"
+      reply(msg, response)
+    end
   end
+
+  # An infinite loop happens when Elix hears itself use the word "guys",
+  # so we need Elix not to reply to itself. The `ignore_from_self?` config
+  # option appears to have been removed so we reproduce it here. With the
+  # Console adapter `user` is a string, but with the Flowdock adapter `user`
+  # is a `Hedwig.User` struct, hence the pattern matching.
+  defp from_self?(%Hedwig.Message{user: %Hedwig.User{name: "Elix"}}), do: true
+  defp from_self?(%Hedwig.Message{user: "Elix"}), do: true
+  defp from_self?(%Hedwig.Message{}), do: false
 end
