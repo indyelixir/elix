52a73d72b7fa19ec1b825d26d0fd2d391d5db254
Steve Grossi <me@stevegrossi.com>
2016-10-16 15:11:45 -0400

    List item manipulation

--- lib/elix/lists.ex
+++ lib/elix/lists.ex
@@ -1,20 +1,44 @@
 defmodule Elix.Lists do
 
   @namespace "lists"
+  @process :redix
 
   def all do
-    {:ok, lists} = Redix.command(:redix, ["LRANGE", @namespace, 0, -1])
-    lists
+    Redix.command!(@process, ["LRANGE", @namespace, 0, -1])
   end
 
   def create(list_name) do
-    {:ok, _length} = Redix.command(:redix, ["RPUSH", @namespace, list_name])
-    all
+    Redix.command!(@process, ["RPUSH", @namespace, list_name])
   end
 
   def delete(list_name) do
-    {:ok, _length} = Redix.command(:redix, ["LREM", @namespace, 0, list_name])
-    all
+    Redix.command!(@process, ["LREM", @namespace, 0, list_name])
+    clear_items(list_name)
   end
 
+  def get_items(list_name) do
+    Redix.command!(@process, ["LRANGE", list_key(list_name), 0, -1])
+  end
+
+  def add_item(list_name, item_name) do
+    Redix.command!(@process, ["RPUSH", list_key(list_name), item_name])
+  end
+
+  def delete_item(list_name, item_name) do
+    Redix.command!(@process, ["LREM", list_key(list_name), 0, item_name])
+  end
+
+  def clear_items(list_name) do
+    Redix.command!(@process, ["DEL", list_key(list_name)])
+  end
+
+  defp list_key(list_name) do
+    "#{@namespace}:#{hex_digest(list_name)}"
+  end
+
+  defp hex_digest(name) do
+    :crypto.hash(:md5, name)
+    |> Base.encode16
+    |> String.downcase
+  end
 end
--- lib/elix/responders/lists.ex
+++ lib/elix/responders/lists.ex
@@ -8,7 +8,7 @@ defmodule Elix.Responders.Lists do
   alias Elix.Lists
 
   @usage """
-  show lists <term> - Displays all lists
+  show lists - Displays all lists
   """
   respond ~r/show lists\Z/i, msg do
     reply(msg, render_items(Lists.all))
@@ -18,22 +18,68 @@ defmodule Elix.Responders.Lists do
   create list <name> - Creates a new list with name
   """
   respond ~r/create list (.+)/i, msg do
-    Lists.create(msg.matches[1])
-    reply(msg, render_items(Lists.all))
+    all_lists = Lists.create(msg.matches[1])
+    reply(msg, render_items(all_lists))
+  end
+
+  @usage """
+  show list <name> - Displays the contents of a list by name
+  """
+  respond ~r/show list (.+)/i, msg do
+    list_name = msg.matches[1]
+
+    reply(msg, render_list(list_name))
   end
 
   @usage """
   delete list <name> - Deletes a list by name
   """
   respond ~r/delete list (.+)/i, msg do
-    Lists.delete(msg.matches[1])
-    reply(msg, render_items(Lists.all))
+    all_lists = Lists.delete(msg.matches[1])
+
+    reply(msg, render_items(all_lists))
   end
 
-  def render_items(list) when is_list(list) do
+  @usage """
+  clear list <name> - Deletes all items from a list by name
+  """
+  respond ~r/clear list (.+)/i, msg do
+    list_name = msg.matches[1]
+    Lists.clear_items(list_name)
+
+    reply(msg, render_list(list_name))
+  end
+
+  @usage """
+  add <item> to <name> - Adds an item to a list by name
+  """
+  respond ~r/add (.+) to (.+)/i, msg do
+    item_name = msg.matches[1]
+    list_name = msg.matches[2]
+    Lists.add_item(list_name, item_name)
+
+    reply(msg, render_list(list_name))
+  end
+
+  @usage """
+  delete <item> from <name> - Deletes an item from a list by name
+  """
+  respond ~r/delete (.+) from (.+)/i, msg do
+    item_name = msg.matches[1]
+    list_name = msg.matches[2]
+    Lists.delete_item(list_name, item_name)
+
+    reply(msg, render_list(list_name))
+  end
+
+  defp render_items(list) when is_list(list) do
     list
     |> Enum.with_index(1)
     |> Enum.map(fn({item, index}) -> "#{index}. #{item}\n" end)
     |> Enum.join
+  end
+
+  defp render_list(list_name) when is_binary(list_name) do
+    "**#{list_name}**\n\n#{render_items(Lists.get_items(list_name))}"
   end
 end
