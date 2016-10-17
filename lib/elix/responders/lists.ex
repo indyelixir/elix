787a5fd5f261c15e1247386531489663ceb257b6
Steve Grossi <me@stevegrossi.com>
2016-10-17 19:27:09 -0400

    Match all the patterns!

--- lib/elix/responders/lists.ex
+++ lib/elix/responders/lists.ex
@@ -4,6 +4,7 @@ defmodule Elix.Responders.Lists do
   """
 
   use Hedwig.Responder
+  alias Hedwig.Message
   alias Elix.List
 
   @usage """
@@ -16,58 +17,47 @@ defmodule Elix.Responders.Lists do
   @usage """
   create list <name> - Creates a new list with name
   """
-  respond ~r/create list (.+)/i, msg do
-    List.create(msg.matches[1])
+  respond ~r/create list (.+)/i, %Message{matches: %{1 => list_name}} = msg do
+    List.create(list_name)
     reply(msg, render_items(List.all))
   end
 
   @usage """
   show list <name> - Displays the contents of a list by name
   """
-  respond ~r/show list (.+)/i, msg do
-    list_name = msg.matches[1]
-
+  respond ~r/show list (.+)/i, %Message{matches: %{1 => list_name}} = msg do
     reply(msg, render_list(list_name))
   end
 
   @usage """
   delete list <name> - Deletes a list by name
   """
-  respond ~r/delete list (.+)/i, msg do
-    List.delete(msg.matches[1])
-
+  respond ~r/delete list (.+)/i, %Message{matches: %{1 => list_name}} = msg do
+    List.delete(list_name)
     reply(msg, render_items(List.all))
   end
 
   @usage """
   clear list <name> - Deletes all items from a list by name
   """
-  respond ~r/clear list (.+)/i, msg do
-    list_name = msg.matches[1]
+  respond ~r/clear list (.+)/i, %Message{matches: %{1 => list_name}} = msg do
     List.clear_items(list_name)
-
     reply(msg, render_list(list_name))
   end
 
   @usage """
   add <item> to <name> - Adds an item to a list by name
   """
-  respond ~r/add (.+) to (.+)/i, msg do
-    item_name = msg.matches[1]
-    list_name = msg.matches[2]
+  respond ~r/add (.+) to (.+)/i, %Message{matches: %{1 => item_name, 2 => list_name}} = msg do
     List.add_item(list_name, item_name)
-
     reply(msg, render_list(list_name))
   end
 
   @usage """
   delete <item> from <name> - Deletes an item from a list by name
   """
-  respond ~r/delete (.+) from (.+)/i, msg do
-    item_name = msg.matches[1]
-    list_name = msg.matches[2]
+  respond ~r/delete (.+) from (.+)/i, %Message{matches: %{1 => item_name, 2 => list_name}} = msg do
     List.delete_item(list_name, item_name)
-
     reply(msg, render_list(list_name))
   end
 
