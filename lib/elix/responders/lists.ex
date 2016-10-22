91d4c324d7bade98a67a8eb0a70e1c4a2d855e6d
Steve Grossi <me@stevegrossi.com>
2016-10-22 07:55:05 -0400

    Refactor for DRYness, though I still don’t think…
    …I’ve got the right boundaries/responsibilities figured out for these modules

--- lib/elix/responders/lists.ex
+++ lib/elix/responders/lists.ex
@@ -26,11 +26,7 @@ defmodule Elix.Responders.Lists do
   show list <list> - Displays the contents of a list by name or number
   """
   respond ~r/show list (.+)/i, %Message{matches: %{1 => list_id}} = msg do
-    list_name = if Regex.match?(~r/\A\d+\Z/, list_id) do
-      list_id |> String.to_integer |> Lists.get_name
-    else
-      list_id
-    end
+    list_name = parse_list_identifier(list_id)
 
     reply(msg, render_list(list_name))
   end
@@ -39,11 +35,7 @@ defmodule Elix.Responders.Lists do
   delete list <list> - Deletes a list by name or number
   """
   respond ~r/delete list (.+)/i, %Message{matches: %{1 => list_id}} = msg do
-    list_name = if Regex.match?(~r/\A\d+\Z/, list_id) do
-      list_id |> String.to_integer |> Lists.get_name
-    else
-      list_id
-    end
+    list_name = parse_list_identifier(list_id)
 
     Lists.delete(list_name)
     reply(msg, render_items(Lists.all))
@@ -53,11 +45,7 @@ defmodule Elix.Responders.Lists do
   clear list <list> - Deletes all items from a list by name or number
   """
   respond ~r/clear list (.+)/i, %Message{matches: %{1 => list_id}} = msg do
-    list_name = if Regex.match?(~r/\A\d+\Z/, list_id) do
-      list_id |> String.to_integer |> Lists.get_name
-    else
-      list_id
-    end
+    list_name = parse_list_identifier(list_id)
 
     Lists.clear_items(list_name)
     reply(msg, render_list(list_name))
@@ -67,11 +55,7 @@ defmodule Elix.Responders.Lists do
   add <item> to <list> - Adds an item to a list by name or number
   """
   respond ~r/add (.+) to (.+)/i, %Message{matches: %{1 => item_name, 2 => list_id}} = msg do
-    list_name = if Regex.match?(~r/\A\d+\Z/, list_id) do
-      list_id |> String.to_integer |> Lists.get_name
-    else
-      list_id
-    end
+    list_name = parse_list_identifier(list_id)
 
     Lists.add_item(list_name, item_name)
     reply(msg, render_list(list_name))
@@ -81,20 +65,31 @@ defmodule Elix.Responders.Lists do
   delete <item> from <name> - Deletes an item from a list by name or number
   """
   respond ~r/delete (.+) from (.+)/i, %Message{matches: %{1 => item_id, 2 => list_id}} = msg do
-    list_name = if Regex.match?(~r/\A\d+\Z/, list_id) do
+    list_name = parse_list_identifier(list_id)
+    item_name = parse_item_identifier(item_id, list_name)
+
+    Lists.delete_item(list_name, item_name)
+    reply(msg, render_list(list_name))
+  end
+
+  defp parse_list_identifier(list_id) do
+    if numeric_string?(list_id) do
       list_id |> String.to_integer |> Lists.get_name
     else
       list_id
     end
+  end
 
-    item_name = if Regex.match?(~r/\A\d+\Z/, item_id) do
+  defp parse_item_identifier(item_id, list_name) do
+    if numeric_string?(item_id) do
       item_id |> String.to_integer |> Lists.get_item_name(list_name)
     else
       item_id
     end
+  end
 
-    Lists.delete_item(list_name, item_name)
-    reply(msg, render_list(list_name))
+  defp numeric_string?(string) do
+    Regex.match?(~r/\A\d+\Z/, string)
   end
 
   defp render_items(list) when is_list(list) do
