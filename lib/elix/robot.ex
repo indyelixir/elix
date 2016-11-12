b187f76e936ea4dab7f99f01a3413863d8c1ee1f
Steve Grossi <me@stevegrossi.com>
2016-11-12 06:58:46 -0500

    Update hedwig and hedwig_flowdock
    And re-run `hedwig.gen.robot` since it includes new GenServer related code like process registration!

--- lib/elix/robot.ex
+++ lib/elix/robot.ex
@@ -1,3 +1,11 @@
 defmodule Elix.Robot do
   use Hedwig.Robot, otp_app: :elix
+
+  def handle_connect(state) do
+    if is_nil(Process.whereis(__MODULE__)) do
+      true = Process.register(self(), __MODULE__)
+    end
+    {:ok, state}
+  end
+
 end
