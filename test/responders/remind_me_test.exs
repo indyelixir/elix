8dd652c9745bee4972d6cf042be91d123fae40df
Steve Grossi <me@stevegrossi.com>
2016-11-12 05:44:54 -0500

    Extract Elix.MessageHelpers
    I should figure out how to use this in all tests. I tried making my own
    
    ```elixir
    defmodule Elix.RobotCase
      def __using__ do
        quote do
          use Hedwig.RobotCase
          # my custom stuff
        end
      end
    end
    ```
    
    But it couldn’t find the Hedwig stuff. `import`ing is good enough for now

--- test/responders/remind_me_test.exs
+++ test/responders/remind_me_test.exs
@@ -1,26 +1,15 @@
 defmodule Elix.Responders.RemindMeTest do
   use Hedwig.RobotCase
-
-  @user_name "testuser"
-  @bot_name "Elix"
+  import Elix.MessageHelpers
 
   describe "Elix.Responders.RemindMe" do
 
-    @tag start_robot: true, name: "elix", responders: [{Elix.Responders.RemindMe, []}]
+    @tag start_robot: true, name: bot_name, responders: [{Elix.Responders.RemindMe, []}]
     test "responds with the reminder", %{adapter: adapter, msg: msg} do
       send adapter, {:message, %{msg | text: to_bot("remind me to eat in 15 minutes")}}
 
       assert_receive {:message, %{text: text}}
       assert text == to_user "Okay, I’ll remind you to eat in 15 minutes"
     end
-  end
-
-  # TODO: DRY me
-  defp to_bot(text) when is_binary(text) do
-    @bot_name <> " " <> text
-  end
-
-  defp to_user(string) when is_binary(string) do
-    @user_name <> ": " <> string
   end
 end
