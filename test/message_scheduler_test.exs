b4e651d07ac874c4727aa91afd164fab26dac201
Steve Grossi <me@stevegrossi.com>
2016-11-19 13:57:19 -0500

    Test stub for MessageScheduler

--- test/message_scheduler_test.exs
+++ test/message_scheduler_test.exs
@@ -1,4 +1,14 @@
 defmodule Elix.MessageSchedulerTest do
   use ExUnit.Case
   doctest Elix.MessageScheduler
+
+  test "sends a message" do
+    assert Elix.MessageScheduler.send_at(now(), :test) == :ok
+
+    # Actually testing this module will be a challenge...
+  end
+
+  defp now do
+    :os.system_time(:seconds)
+  end
 end
