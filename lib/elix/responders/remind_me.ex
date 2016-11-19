b9d227b4fa09a55044d23b8891ba55574b5eebda
Steve Grossi <me@stevegrossi.com>
2016-11-19 05:53:31 -0500

    Nicer reminder message

--- lib/elix/responders/remind_me.ex
+++ lib/elix/responders/remind_me.ex
@@ -9,7 +9,7 @@ defmodule Elix.Responders.RemindMe do
   @usage """
   remind me to <thing> in <time> - Sets a reminder
   """
-  hear ~r/remind me to (.+) in (.+)/i, %Message{
+  hear ~r/remind me (.+) in (.+)/i, %Message{
                                          matches: %{
                                            1 => subject,
                                            2 => time_string
@@ -19,8 +19,10 @@ defmodule Elix.Responders.RemindMe do
 
     seconds_from_now = Elix.TimeParser.from_now(time_string)
     remind_at_timestamp = :os.system_time(:seconds) + seconds_from_now
-    Elix.Reminder.enqueue(subject, remind_at_timestamp, user_name)
 
-    reply(msg, "Okay, I’ll remind you to #{subject} in #{time_string}.")
+    future_reply = "You asked me to remind you #{subject}."
+    Elix.Reminder.enqueue(future_reply, remind_at_timestamp, user_name)
+
+    reply(msg, "Okay, I’ll remind you #{subject} in #{time_string}.")
   end
 end
