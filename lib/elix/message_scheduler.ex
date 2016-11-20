6d85a4f6ec36d25309e3d4aeca82b1806d76faaa
Steve Grossi <me@stevegrossi.com>
2016-11-20 09:16:31 -0500

    Persist in Redis to survive crashes
    I created a `cast` for getting the initial store from redis so as not to slow down the `init` callback, which is synchronous. As long as we don’t start the heartbeat until after initializing the store, we should be good.
    
    You can test in the console by closing and opening another chat console: you’ll still be reminded.

--- lib/elix/message_scheduler.ex
+++ lib/elix/message_scheduler.ex
@@ -4,12 +4,14 @@ defmodule Elix.MessageScheduler do
   """
   use GenServer
 
+  @namespace "scheduled_messages"
+
   def start_link do
     GenServer.start_link(__MODULE__, [], [name: __MODULE__])
   end
 
   def init(state) do
-    heartbeat()
+    GenServer.cast(__MODULE__, :init)
     {:ok, state}
   end
 
@@ -25,16 +27,38 @@ defmodule Elix.MessageScheduler do
     GenServer.cast(__MODULE__, {:schedule, {message, timestamp}})
   end
 
-  def handle_cast({:schedule, scheduled_message}, state) do
+  def handle_cast(:init, _state) do
+    state_from_store =
+      Redix.command!(:redix, ["ZRANGE", @namespace, 0, -1, "WITHSCORES"])
+      |> Stream.chunk(2)
+      |> Enum.map(fn([binary_message, timestamp_string]) ->
+           {:erlang.binary_to_term(binary_message), String.to_integer(timestamp_string)}
+         end)
+
+    heartbeat()
+    {:noreply, state_from_store}
+  end
+
+  def handle_cast({:schedule, {message, timestamp} = scheduled_message}, state) do
+    message_bin = :erlang.term_to_binary(message)
+    Redix.command!(:redix, ["ZADD", @namespace, timestamp, message_bin])
+
     {:noreply, [scheduled_message | state]}
   end
 
+  def handle_info(:heartbeat, []) do
+    # Do nothing if state is empty
+    heartbeat()
+    {:noreply, []}
+  end
   def handle_info(:heartbeat, state) do
     new_state =
       state
       |> Stream.map(fn ({message, timestamp} = scheduled_message) ->
            if timestamp <= :os.system_time(:seconds) do
              GenServer.cast(Elix.Robot, message)
+             message_bin = :erlang.term_to_binary(message)
+             Redix.command!(:redix, ["ZREM", @namespace, message_bin])
              nil
            else
              scheduled_message
