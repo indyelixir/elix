b4ed07e2d27c1990149c1b39ff8250c7b222fd41
Steve Grossi <me@stevegrossi.com>
2016-12-10 08:15:50 -0500

    Begin testing

--- test/message_scheduler_test.exs
+++ test/message_scheduler_test.exs
@@ -2,10 +2,30 @@ defmodule Elix.MessageSchedulerTest do
   use ExUnit.Case
   doctest Elix.MessageScheduler
 
+  # Helpful?
+  # http://joshnuss.blogspot.com/2015/10/testing-messages-with-elixir.html
+
+  defmodule Forwarder do
+    use GenServer
+
+    def start_link(recipient) do
+      GenServer.start_link(__MODULE__, recipient)
+    end
+
+    def init(recipient) do
+      {:ok, recipient}
+    end
+
+    def handle_cast(message, recipient) do
+      send(recipient, message)
+    end
+  end
+
   test "sends a message" do
     assert Elix.MessageScheduler.send_at(now(), :test) == :ok
 
-    # Actually testing this module will be a challenge...
+    Forwarder
+    # assert_receive :test, 2_000
   end
 
   defp now do
--- test/responders/remind_me_test.exs
+++ test/responders/remind_me_test.exs
@@ -2,9 +2,10 @@ defmodule Elix.Responders.RemindMeTest do
   use Hedwig.RobotCase
   import Elix.MessageHelpers
 
+  @moduletag start_robot: true, name: bot_name, responders: [{Elix.Responders.RemindMe, []}]
+
   describe "Elix.Responders.RemindMe" do
 
-    @tag start_robot: true, name: bot_name, responders: [{Elix.Responders.RemindMe, []}]
     test "responds with the reminder", %{adapter: adapter, msg: msg} do
       send adapter, {:message, %{msg | text: to_bot("remind me to eat in 15 seconds")}}
 
@@ -12,7 +13,6 @@ defmodule Elix.Responders.RemindMeTest do
       assert text == to_user "Okay, I’ll remind you to eat in 15 seconds."
     end
 
-    @tag start_robot: true, name: bot_name, responders: [{Elix.Responders.RemindMe, []}]
     test "is helpful about parse errors", %{adapter: adapter, msg: msg} do
       send adapter, {:message, %{msg | text: to_bot("remind me to explode in WAIT NO NEVER")}}
 
