d9ec0cf6873a8011cbf5e21ab2599ed907865574
Steve Grossi <me@stevegrossi.com>
2017-01-22 12:33:46 -0500

    Get list tests passing
    neat that setup works for doctests, too!

--- test/list_test.exs
+++ test/list_test.exs
@@ -1,4 +1,17 @@
 defmodule Elix.ListTest do
   use ExUnit.Case, async: true
+  alias Elix.Brain
+
   doctest Elix.List
+
+  setup do
+    [{:ok, _} = Brain.start_link(
+      %{
+        "lists" => ["Groceries", "PLIBMTLBHGATY", "Places to Visit"],
+        "lists:groceries" => ["platypus milk"],
+        "lists:plibmtlbhgaty" => [],
+        "lists:places-to-visit" => ["Indianapolis", "The Moon", "Space"]
+      }
+    )]
+  end
 end
